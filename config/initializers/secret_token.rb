d408623fcc2ece9323f78c3adb26ea880dc8d3c2
Ruwan Abeyesekera <ruwancode@gmail.com>
2012-08-20 14:40:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppWPsql::Application.config.secret_token = 'fab44e39fb3b22ba23db0d05524d9fa4617e9ecccb820ac277c6e828ac0b7b01e629e79516d2ebe4ec96f21c4476ad8e6ff39f02a1bde44e41d7337e33b55ef0'
